d308e265f84cfaadb26191ca8be1d0ca825ab5c4
Michael Burton <michael@MacBook-Pro.home.gateway>
2015-06-23 21:28:33 +0100

    Contextual jquery added

--- src/main.js
+++ src/main.js
@@ -1,37 +1,74 @@
 var rcApp = rcApp || {};
 
-require.config({
-	baseUrl: '',
-	paths: {
+var jQuery = window.jQuery,
+    // check for old versions of jQuery
+    oldjQuery = jQuery && !!jQuery.fn.jquery.match(/^1\.[0-6](\.|$)/),
+    localJqueryPath = 'https://code.jquery.com/jquery-1.11.3.min',
+	paths = {
 		'translations': 'js/data/translations/en',
 		'handlebars.runtime': 'library/handlebars/handlebars.runtime.amd.min',
 		'moment': 'library/moment/min/moment.min',
 		'pikaday': 'library/pikaday/pikaday',
 		'helpers': 'js/helpers',
-		'template': 'templates/app.tpl'
+		'template': 'templates/app.tpl',
+		'jquery-cascading-dropdown': 'library/jquery-cascading-dropdown/dist/jquery.cascadingdropdown.min'
+	},
+    noConflict;
+
+// check for jQuery 
+if (!jQuery || oldjQuery) {
+    // load if it's not available or doesn't meet min standards
+    paths.jquery = localJqueryPath;
+    noConflict = !!oldjQuery;
+} else {
+    // register the current jQuery
+    define('jquery', [], function() { return jQuery; });
+}
+
+// set up require
+require.config({
+    paths: paths,
+	baseUrl: '',
+	include: ['jquery-cascading-dropdown'],
+	shim: {
+		'jquery-cascading-dropdown': {
+			deps: ['jquery']
+		}
 	}
-})
+});
 
 // load stuff
-require(['moment', 'pikaday', 'translations', 'template', 'helpers'], function(Moment, Pikaday, translations, template, helpers) {
+require(['moment', 'pikaday', 'translations', 'template', 'helpers', 'jquery'], function(Moment, Pikaday, translations, template, helpers, $) {
 
-	var rcApp = rcApp || {};
+	function loadCss(urls) {
+		$.each(urls, function(index, url) {
+			var link = $('<link/>', {
+				type: 'text/css',
+				rel: 'stylesheet',
+				href: url
+			});
+	    	$container.before(link);
+		})
+	}
+
+	if (noConflict) $.noConflict();
 
 	var defaults = {
 		preflang: 'en',
-		translations: {
-		    title: "Search for Car Hire",
-		    country: "Country",
-		    city: "City",
-		    location: "Location",
-		    submit: "Search"
-	    },
-	    countries: []
+		containerId: 'app',
+		dateFormat: 'D MMM YYYY'
 	}
 
-	var html = template(translations);
+	rcApp.options = $.extend(true, {}, defaults, rcApp.options);
 
-	document.getElementById('app').innerHTML = html;
+	var $container = $('#' + rcApp.options.containerId);
+
+	loadCss([
+		'library/cleanslate/cleanslate.css',
+		'library/pikaday/css/pikaday.css'
+	])
+
+	$container.html(template(translations));
 
 	var startDate = new Date();
 
@@ -43,7 +80,7 @@ require(['moment', 'pikaday', 'translations', 'template', 'helpers'], function(M
 		minDate: startDate,
 		setDefaultDate: true,
 	    field: document.getElementById('rc-datepicker--pickup'),
-	    format: 'D MMM YYYY',
+	    format: rcApp.options.dateFormat,
 	    onSelect: function() {
 	    	var date = this.getMoment();
 	    	dropoffDate.setMinDate(date);
@@ -56,7 +93,7 @@ require(['moment', 'pikaday', 'translations', 'template', 'helpers'], function(M
 		minDate: startDate,
 		setDefaultDate: true,
 	    field: document.getElementById('rc-datepicker--dropoff'),
-	    format: 'D MMM YYYY',
+	    format: rcApp.options.dateFormat,
 	    onSelect: function() {
 	    }
 	});
