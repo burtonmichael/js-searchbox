5dc26f3fcc967046768d6bae115bb79947b3de1f
Michael Burton <michael@MacBook-Pro.home.gateway>
2015-06-24 18:13:45 +0100

    Trygin again for r.js via Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,8 +11,29 @@ module.exports = function(grunt) {
                     'src/templates/compiled/app.tpl.js': 'src/templates/*.hbs'
                 }
             }
+        },
+        requirejs: {
+            options: {
+                baseUrl: "./src",
+                dir: "./dist",
+                name: "main",
+                paths: {
+                    'moment': '../bower_components/moment/min/moment.min',
+                    'handlebars.runtime': '../bower_components/handlebars/handlebars.runtime.amd.min',
+                    'pikaday': '../bower_components/pikaday/pikaday',
+                    'template': 'templates/compiled/app.tpl',
+                    'translations': 'js/data/translations/es'
+                },
+                shim: {
+                    'pikaday': {
+                        deps: ['moment']
+                    }
+                },
+                optimizeCss: 'default'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-handlebars');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
 }
